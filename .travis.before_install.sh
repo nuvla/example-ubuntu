c8572a755730d33a538328001009e31e9f261a5e
Charles Loomis <cal@sixsq.com>
2019-06-02 09:44:34 +0200

    try serverless multi-arch build

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -2,14 +2,14 @@
 
 sudo docker run --privileged linuxkit/binfmt:v0.6
 
-sudo docker run -d --privileged \
-     -p 1234:1234 \
-     --name buildkit \
-     moby/buildkit:latest \
-     --addr tcp://0.0.0.0:1234 \
-     --oci-worker-platform linux/amd64 \
-     --oci-worker-platform linux/arm64
+#sudo docker run -d --privileged \
+#     -p 1234:1234 \
+#     --name buildkit \
+#     moby/buildkit:latest \
+#     --addr tcp://0.0.0.0:1234 \
+#     --oci-worker-platform linux/amd64 \
+#     --oci-worker-platform linux/arm64
 
-sudo docker cp buildkit:/usr/bin/buildctl /usr/bin/
+#sudo docker cp buildkit:/usr/bin/buildctl /usr/bin/
 
-export BUILDKIT_HOST=tcp://0.0.0.0:1234
+#export BUILDKIT_HOST=tcp://0.0.0.0:1234
--- .travis.script.sh
+++ .travis.script.sh
@@ -12,23 +12,29 @@ DOCKER_TAG="latest"
 unset HISTFILE
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 
-buildctl build \
-         --frontend dockerfile.v0 \
-         --opt platform=linux/${PLATFORM_1} \
-         --opt filename=${DOCKERFILE_LOCATION} \
-         --output type=image,name=docker.io/${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}-${PLATFORM_1},push=true \
-         --local dockerfile=. \
-         --local context=. \
-         --progress plain
+docker run -it --rm --privileged -v /path/to/dir:/tmp/work --entrypoint buildctl-daemonless.sh moby/buildkit:master \
+       build \
+       --frontend dockerfile.v0 \
+       --opt platform=linux/${PLATFORM_1} \
+       --opt filename=${DOCKERFILE_LOCATION} \
+       --output type=image,name=docker.io/${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}-${PLATFORM_1},push=true \
+       --local context=/tmp/work \
+       --local dockerfile=/tmp/work \
+       --oci-worker-platform linux/amd64 \
+       --oci-worker-platform linux/arm64 \
+       --progress plain
 
-buildctl build \
-         --frontend dockerfile.v0 \
-         --opt platform=linux/${PLATFORM_2} \
-         --opt filename=${DOCKERFILE_LOCATION} \
-         --output type=image,name=docker.io/${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}-${PLATFORM_2},push=true \
-         --local dockerfile=. \
-         --local context=. \
-         --progress plain
+docker run -it --rm --privileged -v /path/to/dir:/tmp/work --entrypoint buildctl-daemonless.sh moby/buildkit:master \
+       build \
+       --frontend dockerfile.v0 \
+       --opt platform=linux/${PLATFORM_2} \
+       --opt filename=${DOCKERFILE_LOCATION} \
+       --output type=image,name=docker.io/${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}-${PLATFORM_2},push=true \
+       --local context=/tmp/work \
+       --local dockerfile=/tmp/work \
+       --oci-worker-platform linux/amd64 \
+       --oci-worker-platform linux/arm64 \
+       --progress plain
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
